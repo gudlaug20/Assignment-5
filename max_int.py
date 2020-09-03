59672ce45004af98fbc80440ec9801a16fd2a084
gudlaug <gudlaug.gudmunds@gmail.com>
2020-09-03 11:10:26 +0000

    skrifaði kóðann og keyrði. Lagfærði hann síðan eftir villumeldingum inná mímí

--- max_int.py
+++ max_int.py
@@ -1,3 +1,12 @@
-Hello world
-asfdhdh
-
+listi = []
+max_int = 0
+num_int = 0
+while num_int >= 0:
+    listi.append(num_int)
+    max_int = max(listi)
+    num_int = int(input("Input a number: ")) 
+print("The maximum is", max_int)
+#Bý til while lykkju
+#Bý til lista til að geta safnað inputunum
+#Ef að tala er lægri eða jafnt og núll þá hættir
+#loopan að taka inn tölur
